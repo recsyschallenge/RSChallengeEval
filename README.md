5fc3237201528dd270e3acc91d3719d7753e1f8e
Alan Said <alan@alans.se>
2014-05-22 18:17:29 +0200

    Evaluator made runnable programmatically

--- README.md
+++ README.md
@@ -4,16 +4,45 @@ This repository contains the official evaluation for the [ACM RecSys Challenge 2
 
 The program calculates normalized [Discounted Cumulative Gain](http://recsyswiki.com/wiki/Discounted_Cumulative_Gain "DCG") at 10 (nDCG@10) given an input of ranking predictions and true ranking.
 
-Running Evaluation
+Running the evaluator as a standalone program
 ------------------
 The evaluator is a Java program. To run it, download the [compiled jar](https://github.com/recsyschallenge/RSChallengeEval/blob/master/target/rscevaluator-0.1-SNAPSHOT-jar-with-dependencies.jar) and execute it like:
 ```bash
 java -jar rscevaluator-0.1-SNAPSHOT-jar-with-dependencies.jar "/path/to/testfile.dat" "/path/to/predictions"
 ```
 
+Using the evaluator from your Java program
+---------------------------------------
+You will need to compile your program with the evaluator in the classpath.
+Compiling:
+```bash
+javac -cp ./rscevaluator-0.1-jar-with-dependencies.jar Example.java
+```
+Executing:
+```bash
+java -cp ./rscevaluator-0.1-jar-with-dependencies.jar:. Example
+´´´
+
+An example class using the evaluator:
+```Java
+import com.recsyschallenge.evaluate.Evaluator;
+import java.io.IOException;
+
+public class Example{
+	public static void main(String args[]){
+		try {
+			Evaluator evaluator = new Evaluator("/path/to/testset.csv", "/path/to/predictions.csv");
+			System.out.println(evaluator.evaluate());
+		} catch (IOException e){
+			System.out.println("Incorrect file paths.");
+		}
+	}
+}
+```
+
 Building jar with dependencies
 ------------------------------
-Should you want to build the jar yourself, you will need:
+Should you want to build the evaluator jar yourself, you will need:
 - [Maven](http://maven.apache.org/)
 - Compiled and installed [rival](http://rival.recommenders.net) into your local Maven repository.
 
--- src/main/java/com/recsyschallenge/evaluate/Evaluator.java
+++ src/main/java/com/recsyschallenge/evaluate/Evaluator.java
@@ -2,7 +2,6 @@ package com.recsyschallenge.evaluate;
 
 import net.recommenders.rival.core.SimpleParser;
 import net.recommenders.rival.core.DataModel;
-import net.recommenders.rival.evaluation.metric.EvaluationMetric;
 import net.recommenders.rival.evaluation.metric.NDCG;
 
 import java.io.File;
@@ -13,45 +12,94 @@ import java.io.IOException;
  */
 public class Evaluator {
 
+
+    /**
+     * The NDCG object used to evaluate the predictions.
+     */
+    public NDCG ndcg;
+
+    /**
+     * The data model based on the testset.
+     */
+    public DataModel test;
+
+    /**
+     * The data model based on the predictions.
+     */
+    public DataModel predictions;
+
+    /**
+     * Constructor for the evaluator.
+     * @param testsetPath The path to the file containing the testset.
+     * @param predictionsetPath The path to the file containing the predictions.
+     * @throws IOException When the files at either @testsetPath or @predictionsetPath do not exist.
+     */
+    public Evaluator(String testsetPath, String predictionsetPath) throws IOException{
+        File testFile = new File(testsetPath);
+        File predictionFile = new File(predictionsetPath);
+        SimpleParser testParser = new SimpleParser();
+        this.test = testParser.parseData(testFile, ",");
+        this.predictions = testParser.parseData(predictionFile, ",");
+        this.ndcg = new NDCG(predictions, test, new int[]{10});
+    }
+
+    /**
+     * Main method of the evaluator. Generates command line output containing the nDCG and the number of users in the testset and predictions.
+     * @param args command line arguments containing the paths to the file containing the testset (args[0]) and the predictions (args[1]).
+     */
     public static void main(String[] args) {
         System.out.println("RecSys Challenge 2014 Evaluator");
         System.out.println("-------------------------------\n");
 
         if (args.length < 2){
             System.out.println("No or too few arguments given!\n");
-
             System.out.println("Usage:\n------");
             System.out.println("The program takes two arguments as input.\n \t1: The test file given by organizers\n \t2: Your predictions\n\n");
-
-            System.out.println("The arguments should be in the form: /path/to/file.dat");
-
+            System.out.println("The arguments should be in the form: /path/to/file.dat\n");
+            System.out.println("Example:");
+            System.out.println("$ java -jar rscevaluator-0.1-jar-with-dependencies.jar /data/recsyschallenge/test_solution.dat /data/recsyschallenge/predictions.dat");
             System.exit(0);
         }
-
-        File testFile = new File(args[0]);
-        File predictionFile = new File(args[1]);
-        SimpleParser testParser = new SimpleParser();
-        DataModel test = null;
-        DataModel recs = null;
+        Evaluator evaluator = null;
         try {
-            test = testParser.parseData(testFile, ",");
-            recs = testParser.parseData(predictionFile, ",");
-        } catch (IOException e) {
+            evaluator = new Evaluator(args[0], args[1]);
+        } catch (IOException e){
             System.out.println("The path of either the test or prediction file were incorrect. Double check your paths and run the evaluator again.");
             System.exit(0);
         }
-
-        NDCG ndcg = new NDCG(recs, test, new int[]{10});
-        ndcg.compute();
-        System.out.println("Evaluated " + recs.getNumUsers() + " users with predictions");
-        System.out.println("Validated agains " + test.getNumUsers() + " users in test set\n");
-        int recusers = recs.getNumUsers();
-        int testusers = test.getNumUsers();
-        if (recusers < testusers)
-            System.out.println("There were no predictions for " + (testusers - recusers) + " users");
+        System.out.println("Evaluated " + evaluator.getNumPredictedUsers() + " users with predictions");
+        System.out.println("Validated against " + evaluator.getNumTestUsers() + " users in test set\n");
+        int recUsers = evaluator.getNumPredictedUsers();
+        int testUsers = evaluator.getNumTestUsers();
+        if (recUsers < testUsers)
+            System.out.println("There were no predictions for " + (testUsers - recUsers) + " users");
         System.out.println("----------------------\n");
-        System.out.println("nDCG@10: " + ndcg.getValueAt(10));
+        System.out.println("nDCG@10: " + evaluator.evaluate());
         System.out.println("\n\n");
+    }
 
+    /**
+     * Compute nDCG@10 for the files given in the constructor.
+     * @return  nDCG@10
+     */
+    public double evaluate(){
+        ndcg.compute();
+        return ndcg.getValueAt(10);
+    }
+
+    /**
+     * Get the number of users in the testset.
+     * @return the number of users in the testset.
+     */
+    public int getNumTestUsers(){
+        return test.getNumUsers();
+    }
+
+    /**
+     * Get the number of users in the prediction set.
+     * @return the number of users in the prediction set.
+     */
+    public int getNumPredictedUsers(){
+        return predictions.getNumUsers();
     }
 }
