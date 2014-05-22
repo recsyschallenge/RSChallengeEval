e82c005c7a139599cc4dad14baae83573e3ace86
Alan Said <alan@alans.se>
2014-05-22 11:35:06 +0200

    Updated evaluator

--- src/main/java/com/recsyschallenge/evaluate/Evaluator.java
+++ src/main/java/com/recsyschallenge/evaluate/Evaluator.java
@@ -16,12 +16,15 @@ public class Evaluator {
     public static void main(String[] args) {
         System.out.println("RecSys Challenge 2014 Evaluator");
         System.out.println("-------------------------------\n");
-        System.out.println("Usage:\n------");
-        System.out.println("The program takes two arguments as input. 1: test, 2: predictions\n\n");
 
-        System.out.println("The arguments should be in the form: /path/to/file.dat");
         if (args.length < 2){
-            System.out.println("No or too few arguments given");
+            System.out.println("No or too few arguments given!\n");
+
+            System.out.println("Usage:\n------");
+            System.out.println("The program takes two arguments as input.\n \t1: The test file given by organizers\n \t2: Your predictions\n\n");
+
+            System.out.println("The arguments should be in the form: /path/to/file.dat");
+
             System.exit(0);
         }
 
@@ -31,20 +34,24 @@ public class Evaluator {
         DataModel test = null;
         DataModel recs = null;
         try {
-            recs = testParser.parseData(predictionFile, ",");
             test = testParser.parseData(testFile, ",");
+            recs = testParser.parseData(predictionFile, ",");
         } catch (IOException e) {
             System.out.println("The path of either the test or prediction file were incorrect. Double check your paths and run the evaluator again.");
             System.exit(0);
         }
 
         NDCG ndcg = new NDCG(recs, test, new int[]{10});
-
-        NDCG ndcg1 = new NDCG(recs, test);
-        ndcg1.compute();
         ndcg.compute();
-        System.out.println("Evaluated " + recs.getNumUsers() + " users with predictions against " + test.getNumUsers() + " users in validation set");
+        System.out.println("Evaluated " + recs.getNumUsers() + " users with predictions");
+        System.out.println("Validated agains " + test.getNumUsers() + " users in test set\n");
+        int recusers = recs.getNumUsers();
+        int testusers = test.getNumUsers();
+        if (recusers < testusers)
+            System.out.println("There were no predictions for " + (testusers - recusers) + " users");
+        System.out.println("----------------------\n");
         System.out.println("nDCG@10: " + ndcg.getValueAt(10));
+        System.out.println("\n\n");
 
     }
 }
